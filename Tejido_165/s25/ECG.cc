43b2533cb13ed4c3c620db8f299e3a8f83929134
Lucia-Romero <lromero@ci2b.upv.es>
2024-12-23 19:22:22 +0100

    Update ECG.cc

--- Tejido_165/s25/ECG.cc
+++ Tejido_165/s25/ECG.cc
@@ -23,32 +23,28 @@ void human::ecg_heading() {
 			"CV");
 	fclose(ecg_file);
 
-	//JORDI
+	
 	QTint_file = fopen(qtint_path, "w");
 	fprintf(QTint_file, "%-07s\t%-07s\n", "t", "QTint");
 	fclose(QTint_file);
 }
 
-void human::ecg_save(int i) { //JORDI
+void human::ecg_save(int i) { 
 
-	if (i % skip == 0) { //JORDI
+	if (i % skip == 0) { 
 		ecg_file = fopen(ecg_path, "a");
 		fprintf(ecg_file, "%-016e\t%-07g\t%-07g\t%-07g\n", t + dt, Phi, gradV,
 				CV); //-tsave
 		fclose(ecg_file);
 	}
 
-	/*
-	 //JORDI: lo he modificado para que guarde los QTint en archivos separados.
-	 Así no se interfieren durante simulaciones en paralelo.
-	 */
 
-	if (QTint == CL && i % 10 == 0) //JORDI
-		if (fmod(t + CL, tsave) > 250.0) //JORDI. "fmod(t+CL,tsave)" ERA t-tsave;
+	if (QTint == CL && i % 10 == 0) 
+		if (fmod(t + CL, tsave) > 250.0) . "fmod(t+CL,tsave)" ERA t-tsave;
 			if (fabs(gradV) <= 0.200) {
-				QTint = fmod(t + CL, tsave); //JORDI
-				QTint_file = fopen(qtint_path, "a"); //JORDI
-				fprintf(QTint_file, "%-07g\t%-07g\n", t - QTint, QTint + dt); //JORDI
-				fclose(QTint_file); //JORDI
+				QTint = fmod(t + CL, tsave); 
+				QTint_file = fopen(qtint_path, "a"); 
+				fprintf(QTint_file, "%-07g\t%-07g\n", t - QTint, QTint + dt); 
+				fclose(QTint_file); 
 			}
 }
